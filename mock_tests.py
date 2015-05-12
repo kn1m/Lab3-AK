64e54edcdf06ac06b825efa74df5430de5bd7543
kn1m <peteknysh@gmail.com>
2015-05-12 11:36:02 +0300

    updated mock tests

--- mock_tests.py
+++ mock_tests.py
@@ -1,30 +1,54 @@
 
-from client import client
+from pysimplesoap.client import SoapClient
 from mock import create_autospec, Mock
 import unittest
 
 
 class MockTest(unittest.TestCase):
     def test_create(self):
-        real = client
+        real = SoapClient(
+            location="http://localhost:8008/",
+            action='http://localhost:8008/',  # SOAPAction
+            namespace="http://example.com/sample.wsdl",
+            soap_ns='soap',
+            trace=True,
+            ns=False)
         real.Create = Mock(return_value='Document has not been created.')
         real.Create(doc_name='test', doc_content='<asd>No<asd>')
         real.Create.assert_called_with(doc_name='test', doc_content='<asd>No<asd>')
 
     def test_read(self):
-        real = client
+        real = SoapClient(
+            location="http://localhost:8008/",
+            action='http://localhost:8008/',  # SOAPAction
+            namespace="http://example.com/sample.wsdl",
+            soap_ns='soap',
+            trace=True,
+            ns=False)
         real.Read = Mock(return_value='Document has not been read.')
         real.Read(doc_name='test', doc_content='<asd>No<asd>')
         real.Read.assert_called_with(doc_name='test', doc_content='<asd>No<asd>')
 
     def test_update(self):
-        real = client
+        real = SoapClient(
+            location="http://localhost:8008/",
+            action='http://localhost:8008/',  # SOAPAction
+            namespace="http://example.com/sample.wsdl",
+            soap_ns='soap',
+            trace=True,
+            ns=False)
         real.Update = Mock(return_value='Document has not been updated.')
         real.Update(doc_name='test', doc_content='<asd>No<asd>')
         real.Update.assert_called_with(doc_name='test', doc_content='<asd>No<asd>')
 
     def test_delete(self):
-        real = client
+        real = SoapClient(
+            location="http://localhost:8008/",
+            action='http://localhost:8008/',  # SOAPAction
+            namespace="http://example.com/sample.wsdl",
+            soap_ns='soap',
+            trace=True,
+            ns=False)
         real.Delete = Mock(return_value='Document has not been deleted.')
         real.Delete(doc_name='test', doc_content='<asd>No<asd>')
         real.Delete.assert_called_with(doc_name='test', doc_content='<asd>No<asd>')
